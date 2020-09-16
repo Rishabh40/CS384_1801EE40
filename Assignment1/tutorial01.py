ed1bbabfba362dca19eb566bba0334647be0dd13
Rishabh40 <arishabh106@gmail.com>
2020-09-16 18:41:02 +0530

    My First Basic Python Calculator Done !

---
+++ Assignment1/tutorial01.py
@@ -0,0 +1,29 @@
+# Function to add two numbers
+def add(num1, num2):
+    addition = num1 + num2
+    return addition
+
+# Function to subtract two numbers
+
+
+def subtract(num1, num2):
+    subtraction = num1 - num2
+    return subtraction
+
+# Function to multiply two numbers
+
+
+def multiply(num1, num2):
+    # Multiplication Logic
+    multiplication = num1*num2
+    return multiplication
+
+# Function to divide two numbers
+
+
+def divide(num1, num2):
+    # DivisionLogic
+    if num2 == 0:
+        return 0
+    division = num1/num2
+    return division
