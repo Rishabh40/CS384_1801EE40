c72186e660ff34d90b8a59b1e9723bbecb938852
Rishabh Agarwal <arishabh106@gmail.com>
2020-09-17 12:53:08 +0530

    Added function definations for 4 more Functions

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -40,3 +40,31 @@ def divide(num1, num2):
     else:
         division = 0
     return division
+
+
+# Function to add power function
+# You cant use the inbuilt python function x ** y . Write your own function
+def power(num1, num2):  # num1 ^ num2
+    # DivisionLogic
+    return power
+
+
+# Python 3 program to print GP.  geometric Progression
+# You cant use the inbuilt python function. Write your own function
+def printGP(a, r, n):
+    gp = []
+    return gp
+
+
+# Python 3 program to print AP.  arithmetic Progression
+# You cant use the inbuilt python function. Write your own function
+def printAP(a, d, n):
+    ap = []
+    return ap
+
+
+# Python 3 program to print HP.   Harmonic Progression
+# You cant use the inbuilt python function. Write your own function
+def printHP(a, d, n):
+    hp = []
+    return hp
