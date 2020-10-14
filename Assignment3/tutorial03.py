01815e019a306a55ead56eef23d77dede844b74e
Rishabh Agarwal <arishabh106@gmail.com>
2020-10-15 00:54:48 +0530

    updated gender function now it will create the required folder/files by its own

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import shutil
 import os
 
 
@@ -20,24 +21,35 @@ def email_domain_extract():
 
 def gender():
     # Read csv and process
-    open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/male.csv', 'w').close()
-    open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/female.csv', 'w').close()
+    path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics'
+    if os.path.isdir(path):
+        spe_path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender'
+        if os.path.isdir(spe_path):
+            shutil.rmtree(spe_path)
+        curr_path = os.path.join(path, 'gender')
+        os.mkdir(curr_path)
+    else:
+        parent_dir = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3'
+        curr_path = os.path.join(parent_dir, 'analytics')
+        os.mkdir(curr_path)
+        final_path = os.path.join(curr_path, 'gender')
+        os.mkdir(final_path)
     with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/studentinfo_cs384.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if row[4] == "gender":
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/female.csv', 'a') as female:
+                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/female.csv', 'a', newline='') as female:
                     writer = csv.writer(female)
                     writer.writerow(row)
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/male.csv', 'a') as male:
+                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/male.csv', 'a', newline='') as male:
                     writer = csv.writer(male)
                     writer.writerow(row)
             elif row[4] == "Female":
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/female.csv', 'a') as female:
+                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/female.csv', 'a', newline='') as female:
                     writer = csv.writer(female)
                     writer.writerow(row)
             else:
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/male.csv', 'a') as male:
+                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/male.csv', 'a', newline='') as male:
                     writer = csv.writer(male)
                     writer.writerow(row)
     pass
@@ -62,6 +74,3 @@ def blood_group():
 def new_file_sort():
     # Read csv and process
     pass
-
-
-gender()
