b4b97c7c35638a848cf8819024316f5460dcec4a
Rishabh Agarwal <arishabh106@gmail.com>
2020-10-15 17:51:50 +0530

    Added tester file and provided more felxibility in the code

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -3,6 +3,17 @@ import re
 import shutil
 import os
 import datetime
+os.system('cls')
+
+
+def del_create_analytics_folder():
+    # del the analytics folder including subfolder
+    # mkdir the analytics folder (only mkdir)
+    if os.path.isdir('./analytics'):
+        shutil.rmtree(
+            './analytics')
+    os.mkdir('./analytics')
+    pass
 
 
 def date_validation(day, month, year):
@@ -24,27 +35,27 @@ def course():
 
 def country():
     # Read csv and process
-    path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics'
+    path = './analytics'
     if os.path.isdir(path):
-        spe_path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/country'
+        spe_path = './analytics/country'
         if os.path.isdir(spe_path):
             shutil.rmtree(spe_path)
         curr_path = os.path.join(path, 'country')
         os.mkdir(curr_path)
     else:
-        parent_dir = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3'
+        parent_dir = '.'
         curr_path = os.path.join(parent_dir, 'analytics')
         os.mkdir(curr_path)
         final_path = os.path.join(curr_path, 'country')
         os.mkdir(final_path)
-    with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/studentinfo_cs384.csv', 'r') as file:
+    with open('./studentinfo_cs384.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if(row[0] == 'id'):
                 header = row
             else:
                 country_path = os.path.join(
-                    'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/country', row[2]+'.csv')
+                    './analytics/country', row[2]+'.csv')
                 if not os.path.isfile(country_path):
                     with open(country_path, 'a', newline='') as file:
                         head = csv.writer(file)
@@ -57,20 +68,20 @@ def country():
 
 def email_domain_extract():
     # Read csv and process
-    path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics'
+    path = './analytics'
     if os.path.isdir(path):
-        spe_path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/email_domain'
+        spe_path = './analytics/email_domain'
         if os.path.isdir(spe_path):
             shutil.rmtree(spe_path)
         curr_path = os.path.join(path, 'email_domain')
         os.mkdir(curr_path)
     else:
-        parent_dir = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3'
+        parent_dir = '.'
         curr_path = os.path.join(parent_dir, 'analytics')
         os.mkdir(curr_path)
         final_path = os.path.join(curr_path, 'email_domain')
         os.mkdir(final_path)
-    with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/studentinfo_cs384.csv', 'r') as file:
+    with open('./studentinfo_cs384.csv', 'r') as file:
         reader = csv.reader(file)
         pattern = re.compile(
             r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,18}$')
@@ -82,7 +93,7 @@ def email_domain_extract():
                     temp = row[3][row[3].index('@')+1:]
                     domain = temp[:temp.index('.')]
                     domain_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/email_domain', domain+'.csv')
+                        './analytics/email_domain', domain+'.csv')
                     if not os.path.isfile(domain_path):
                         with open(domain_path, 'a', newline='') as file:
                             head = csv.writer(file)
@@ -92,7 +103,7 @@ def email_domain_extract():
                         data.writerow(row)
                 else:
                     domain_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/email_domain', 'misc.csv')
+                        './analytics/email_domain', 'misc.csv')
                     if not os.path.isfile(domain_path):
                         with open(domain_path, 'a', newline='') as file:
                             head = csv.writer(file)
@@ -105,35 +116,35 @@ def email_domain_extract():
 
 def gender():
     # Read csv and process
-    path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics'
+    path = './analytics'
     if os.path.isdir(path):
-        spe_path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender'
+        spe_path = './analytics/gender'
         if os.path.isdir(spe_path):
             shutil.rmtree(spe_path)
         curr_path = os.path.join(path, 'gender')
         os.mkdir(curr_path)
     else:
-        parent_dir = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3'
+        parent_dir = '.'
         curr_path = os.path.join(parent_dir, 'analytics')
         os.mkdir(curr_path)
         final_path = os.path.join(curr_path, 'gender')
         os.mkdir(final_path)
-    with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/studentinfo_cs384.csv', 'r') as file:
+    with open('./studentinfo_cs384.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if row[4] == "gender":
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/female.csv', 'a', newline='') as female:
+                with open('./analytics/gender/female.csv', 'a', newline='') as female:
                     writer = csv.writer(female)
                     writer.writerow(row)
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/male.csv', 'a', newline='') as male:
+                with open('./analytics/gender/male.csv', 'a', newline='') as male:
                     writer = csv.writer(male)
                     writer.writerow(row)
             elif row[4] == "Female":
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/female.csv', 'a', newline='') as female:
+                with open('./analytics/gender/female.csv', 'a', newline='') as female:
                     writer = csv.writer(female)
                     writer.writerow(row)
             else:
-                with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/gender/male.csv', 'a', newline='') as male:
+                with open('./analytics/gender/male.csv', 'a', newline='') as male:
                     writer = csv.writer(male)
                     writer.writerow(row)
     pass
@@ -141,20 +152,20 @@ def gender():
 
 def dob():
     # Read csv and process
-    path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics'
+    path = './analytics'
     if os.path.isdir(path):
-        spe_path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/dob'
+        spe_path = './analytics/dob'
         if os.path.isdir(spe_path):
             shutil.rmtree(spe_path)
         curr_path = os.path.join(path, 'dob')
         os.mkdir(curr_path)
     else:
-        parent_dir = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3'
+        parent_dir = '.'
         curr_path = os.path.join(parent_dir, 'analytics')
         os.mkdir(curr_path)
         final_path = os.path.join(curr_path, 'dob')
         os.mkdir(final_path)
-    with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/studentinfo_cs384.csv', 'r') as file:
+    with open('./studentinfo_cs384.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if(row[0] == 'id'):
@@ -166,15 +177,15 @@ def dob():
                 year = int(pattern[2])
                 if date_validation(day, month, year):
                     range1_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/dob', 'bday_1995_1999.csv')
+                        './analytics/dob', 'bday_1995_1999.csv')
                     range2_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/dob', 'bday_2000_2004.csv')
+                        './analytics/dob', 'bday_2000_2004.csv')
                     range3_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/dob', 'bday_2005_2009.csv')
+                        './analytics/dob', 'bday_2005_2009.csv')
                     range4_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/dob', 'bday_2010_2014.csv')
+                        './analytics/dob', 'bday_2010_2014.csv')
                     range5_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/dob', 'bday_2015_2020.csv')
+                        './analytics/dob', 'bday_2015_2020.csv')
                     if year >= 1995 and year <= 1999:
                         if not os.path.isfile(range1_path):
                             with open(range1_path, 'a', newline='') as file:
@@ -217,7 +228,7 @@ def dob():
                             data.writerow(row)
                 else:
                     mics_path = os.path.join(
-                        'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/dob', 'misc.csv')
+                        './analytics/dob', 'misc.csv')
                     if not os.path.isfile(mics_path):
                         with open(mics_path, 'a', newline='') as file:
                             head = csv.writer(file)
@@ -230,27 +241,27 @@ def dob():
 
 def state():
     # Read csv and process
-    path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics'
+    path = './analytics'
     if os.path.isdir(path):
-        spe_path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/state'
+        spe_path = './analytics/state'
         if os.path.isdir(spe_path):
             shutil.rmtree(spe_path)
         curr_path = os.path.join(path, 'state')
         os.mkdir(curr_path)
     else:
-        parent_dir = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3'
+        parent_dir = '.'
         curr_path = os.path.join(parent_dir, 'analytics')
         os.mkdir(curr_path)
         final_path = os.path.join(curr_path, 'state')
         os.mkdir(final_path)
-    with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/studentinfo_cs384.csv', 'r') as file:
+    with open('./studentinfo_cs384.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if(row[0] == 'id'):
                 header = row
             else:
                 state_path = os.path.join(
-                    'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/state', row[7]+'.csv')
+                    './analytics/state', row[7]+'.csv')
                 if not os.path.isfile(state_path):
                     with open(state_path, 'a', newline='') as file:
                         head = csv.writer(file)
@@ -263,27 +274,27 @@ def state():
 
 def blood_group():
     # Read csv and process
-    path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics'
+    path = './analytics'
     if os.path.isdir(path):
-        spe_path = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/blood_group'
+        spe_path = './analytics/blood_group'
         if os.path.isdir(spe_path):
             shutil.rmtree(spe_path)
         curr_path = os.path.join(path, 'blood_group')
         os.mkdir(curr_path)
     else:
-        parent_dir = 'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3'
+        parent_dir = '.'
         curr_path = os.path.join(parent_dir, 'analytics')
         os.mkdir(curr_path)
         final_path = os.path.join(curr_path, 'blood_group')
         os.mkdir(final_path)
-    with open('C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/studentinfo_cs384.csv', 'r') as file:
+    with open('./studentinfo_cs384.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if(row[0] == 'id'):
                 header = row
             else:
                 blood_path = os.path.join(
-                    'C:/Users/RISHABH AGARWAL/Downloads/SEM 5/CS384-Python/CS384_1801EE40/Assignment3/analytics/blood_group', row[6]+'.csv')
+                    './analytics/blood_group', row[6]+'.csv')
                 if not os.path.isfile(blood_path):
                     with open(blood_path, 'a', newline='') as file:
                         head = csv.writer(file)
@@ -298,11 +309,3 @@ def blood_group():
 def new_file_sort():
     # Read csv and process
     pass
-
-
-dob()
-email_domain_extract()
-country()
-state()
-gender()
-blood_group()
