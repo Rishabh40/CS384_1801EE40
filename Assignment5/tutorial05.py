fa1623971ab965a09041cfa7b342bd8cdf4183b9
Rishabh Agarwal <arishabh106@gmail.com>
2020-11-14 02:43:58 +0530

    Assignment5- Added rename how i met your mother function !!

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -132,4 +132,45 @@ def rename_Suits(paddingseason, paddingepisode):
     pass
 
 
-rename_Suits(2, 5)
+#rename_Suits(2, 5)
+
+
+def rename_How_I_Met_Your_Mother(paddingseason, paddingepisode):
+    # How I met your mother rename Logic
+    path = r'./Subtitles/How I Met Your Mother'
+    seasonno = ''
+    episodeno = ''
+    episodename = ''
+    for filename in os.listdir(path):
+        lis = filename.split(' - ')
+        seasonno = lis[1][0:lis[1].index('x')]
+        episodeno = lis[1][lis[1].index('x')+1:]
+        temp1 = lis[2].split('.480')
+        temp2 = temp1[0].split('.720')
+        temp3 = temp2[0].split('.1080')
+        temp4 = temp3[0].split('.HDTV')
+        episodename = temp4[0].split('.en')[0]
+        while len(seasonno) < paddingseason:
+            seasonno = '0'+seasonno
+        if len(seasonno) > paddingseason:
+            seasonno = seasonno[-1*paddingseason:]
+        while len(episodeno) < paddingepisode:
+            episodeno = '0'+episodeno
+        if len(episodeno) > paddingepisode:
+            episodeno = episodeno[-1*paddingepisode:]
+        if filename.endswith(".mp4"):
+            if not os.path.isfile(path+'/'+lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.mp4'):
+                os.rename(path+'/'+filename, path+'/' +
+                          lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.mp4')
+            else:
+                os.remove(path+'/'+filename)
+        if filename.endswith('.srt'):
+            if not os.path.isfile(path+'/'+lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.srt'):
+                os.rename(path+'/'+filename, path+'/' +
+                          lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.srt')
+            else:
+                os.remove(path+'/'+filename)
+    pass
+
+
+rename_How_I_Met_Your_Mother(2, 6)
