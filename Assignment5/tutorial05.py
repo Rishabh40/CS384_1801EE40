0a5a2d50ea4a1e0ff6c40d989eda933cdcb05c79
Rishabh Agarwal <arishabh106@gmail.com>
2020-11-14 02:25:02 +0530

    Assignment5- Completed rename FIR function !!

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -1,20 +1,33 @@
+# Importing required Libraries
+import os
+import re
 
-def rename_FIR(folder_name):
-    # rename Logic 
-    
 
-def rename_Game_of_Thrones(folder_name):
-    # rename Logic 
-    
+def rename_FIR(paddingseason, paddingepisode):
+    # FIR rename Logic
+    path = r'./Subtitles/FIR'
+    episodeno = ''
+    for filename in os.listdir(path):
+        pattern = re.compile(r'\d+')
+        list = re.findall(pattern, filename)
+        episodeno = list[0]
+        while len(episodeno) < paddingepisode:
+            episodeno = '0'+episodeno
+        if len(episodeno) > paddingepisode:
+            episodeno = episodeno[-1*paddingepisode:]
+        if filename.endswith(".mp4"):
+            if not os.path.isfile(path+'/'+'FIR'+' - Episode '+episodeno+'.mp4'):
+                os.rename(path+'/'+filename, path+'/'+'FIR' +
+                          ' - Episode '+episodeno+'.mp4')
+            else:
+                os.remove(path+'/'+filename)
+        if filename.endswith('.srt'):
+            if not os.path.isfile(path+'/'+'FIR'+' - Episode '+episodeno+'.srt'):
+                os.rename(path+'/'+filename, path+'/'+'FIR' +
+                          ' - Episode '+episodeno+'.srt')
+            else:
+                os.remove(path+'/'+filename)
+    pass
 
-def rename_Sherlock(folder_name):
-    # rename Logic 
-    
 
-def rename_Suits(folder_name):
-    # rename Logic 
-    
-
-def rename_How_I_Met_Your_Mother(folder_name):
-    # rename Logic 
-    
+rename_FIR(5, 5)
