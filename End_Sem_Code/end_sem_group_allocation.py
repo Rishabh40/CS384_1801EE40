dc7955ef234d9f45d89fb72a344bffb9ece12b14
Rishabh Agarwal <arishabh106@gmail.com>
2020-11-30 02:12:19 +0530

    End Sem - Added prototype of how things will be done in my code!

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -1,8 +1,39 @@
+from math import floor
+import csv
+import os
+import pandas as pd
+import shutil
+
+
+def deletion():
+    # used for deletion of already created data
+
+    return
+
+
+def branch_classification(filename):
+    # this function reads dat from input files and classify that according
+    # to branches and creates csv files for diff. branches with stats file too.
+
+    return
+
+
+def group_classification(number_of_groups, no_of_branches, Data):
+    # this function is used to divide students into given no. of groups
+    # and then finally creating those groups csv's along with stats file
+
+    return
+
 
 def group_allocation(filename, number_of_groups):
-    # Entire Logic 
-	# You can add more functions, but in the test case, we will only call the group_allocation() method,
+    # Entire Logic
+    # You can add more functions, but in the test case, we will only call the group_allocation() method,
+    deletion()
+    no_of_branches, Data = branch_classification(filename)
+    group_classification(number_of_groups, no_of_branches, Data)
+    return
+
 
 filename = "Btech_2020_master_data.csv"
-number_of_groups = 12 
+number_of_groups = 12
 group_allocation(filename, number_of_groups)
