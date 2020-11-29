087f7c39bb9994d7c927b11be9f8c83a61e64671
Rishabh Agarwal <arishabh106@gmail.com>
2020-11-30 02:21:33 +0530

    End Sem - Added code for classification into given no of groups!

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -54,7 +54,53 @@ def branch_classification(filename):
 def group_classification(number_of_groups, no_of_branches, Data):
     # this function is used to divide students into given no. of groups
     # and then finally creating those groups csv's along with stats file
-
+    group_data = [[0 for i in range(no_of_branches+2)]
+                  for j in range(number_of_groups+1)]
+    group_data[0][0] = "group"
+    group_data[0][1] = "total"
+    for i in range(2, no_of_branches+2):
+        group_data[0][i] = Data[i-2][0]
+    padd = len(str(number_of_groups))
+    for i in range(1, number_of_groups+1):
+        zeros = padd-len(str(i))
+        group_data[i][0] = "Group_G"+'0'*zeros+str(i)+".csv"
+    left = []
+    for i in range(len(Data)):
+        entry = int(floor(Data[i][1]/number_of_groups))
+        for j in range(1, number_of_groups+1):
+            group_data[j][i+2] = entry
+        left.append(Data[i][1]-number_of_groups*entry)
+    x = 1
+    for i in range(len(left)):
+        while left[i] > 0:
+            group_data[x][i+2] += 1
+            left[i] -= 1
+            if x == number_of_groups:
+                x = 1
+            else:
+                x += 1
+    for i in range(1, number_of_groups+1):
+        for j in range(2, no_of_branches+2):
+            group_data[i][1] += group_data[i][j]
+    if os.path.isfile(r'./groups/stats_grouping.csv'):
+        os.remove(r'./groups/stats_grouping.csv')
+    with open(r'./groups/stats_grouping.csv', 'w', newline="") as file:
+        mp = csv.writer(file)
+        mp.writerows(group_data)
+    for i in range(1, number_of_groups+1):
+        with open(r'./groups/'+group_data[i][0], 'w', newline="") as file:
+            mp = csv.writer(file)
+            mp.writerow(["Roll", "Name", "Email"])
+    for i in range(2, no_of_branches+2):
+        data = pd.read_csv(r'./groups/'+group_data[0][i]+'.csv')
+        curr = 0
+        for j in range(1, number_of_groups+1):
+            df = data.iloc[curr:curr+group_data[j][i]]
+            rows = df.values.tolist()
+            curr += group_data[j][i]
+            with open(r'./groups/'+group_data[j][0], 'a+', newline="") as file:
+                mp = csv.writer(file)
+                mp.writerows(rows)
     return
 
 
