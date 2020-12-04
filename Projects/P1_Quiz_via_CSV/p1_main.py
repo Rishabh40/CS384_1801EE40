299e23889cd0fed034f11f8b8653229baedeb77a
Rishabh Agarwal <arishabh106@gmail.com>
2020-12-04 20:59:27 +0530

    Project Quiz Portal - Added goto quiz_submit and export_database functions into main file

---
+++ Projects/P1_Quiz_via_CSV/p1_main.py
@@ -0,0 +1,90 @@
+# Rishabh Agarwal
+# 1801EE40
+# Importing Required Modules
+import csv
+import sqlite3
+import datetime
+import os
+import re
+import keyboard
+import time
+from numpy.core.numeric import roll
+import pandas as pd
+import threading
+from tkinter import *
+import shutil
+import bcrypt
+import getpass as gp
+
+# # Please note in responses marked_choices will be blank if time is over or forcefully submitted  # #
+# global variable to determine how the quiz is finished ex- time over,
+# ques over or user chose to finish or the quiz is still goin on
+global flag
+# 1 quiz goin on
+# 2 timer off
+# -1 user forcefully submitted
+# 0 ques over
+
+
+def goto():
+    """   Gives the user a chance to access whatever ques he/she wants   """
+    i = int(input('which question you want to go: '))
+    os.system("cls" if os.name == "nt" else "clear")
+    with open(r'./quiz_wise_questions/'+quiz_number+'.csv', 'r') as file:
+        reader = csv.reader(file)
+        row = list(reader)
+        print("Roll No- ", rollno)
+        print("Name- ", Name)
+        print("Goto Question: (Press - Ctrl+Alt+g)")
+        print("Final Submit: (Press Ctrl+Alt+f)")
+        print("Export Database Into CSV: (Press Ctrl+Alt+e)")
+        print("Ques No ", row[i][0], sep=':-')
+        print(row[i][1], " ?")
+        print("Option 1) ", row[i][2])
+        print("Option 2) ", row[i][3])
+        print("Option 3) ", row[i][4])
+        print("Option 4) ", row[i][5])
+        print("Credits if correct option: ", row[i][7])
+        print("Negative Marking: ", row[i][8])
+        print("Is Compulsory: ", row[i][9])
+    print("Press esc to continue!!")
+    pass
+
+
+def extract_database(username):
+    """  Extracting users data into a csv file  """
+    con = sqlite3.connect("project1_quiz_cs384.db")
+    c = con.cursor()
+    c.execute("SELECT * FROM project1_marks WHERE roll=?", (username,))
+    lst = c.fetchall()
+    header = ['Roll', 'quiz_num', 'marks', 'Time_of_submission']
+    current_time = datetime.datetime.now()
+    for tup in lst:
+        details = list(tup)
+        details.append(current_time)
+        if not os.path.isfile(r'./quiz_wise_responses/scores_'+tup[1]+'.csv'):
+            with open(r'./quiz_wise_responses/scores_'+tup[1]+'.csv', 'w', newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(header)
+        else:
+            input_file = open(
+                r'./quiz_wise_responses/scores_'+tup[1]+'.csv', "r+")
+            reader_file = csv.reader(input_file)
+            value = len(list(reader_file))
+            if value == 0:
+                with open(r'./quiz_wise_responses/scores_'+tup[1]+'.csv', 'w', newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(header)
+
+        with open(r'./quiz_wise_responses/scores_'+tup[1]+'.csv', 'a+', newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(details)
+
+
+def quiz_submit():
+    """  Giving user a chance to submit quiz before time  """
+    global flag
+    if flag == 1:
+        flag = -1
+    print("Quiz Submitted!!")
+    pass
